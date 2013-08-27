06b074b72115b5e5d19cefa03b01ba332d67b21f
Shahar Livne <shahar@cellrox.com>
2013-08-27 12:35:41 +0300

    add libseccomp to build
    translate the makefile to Android.mk and add missing include files
    remark: libseccomp is at version 2.1.0 + python bindings
    
    Change-Id: I177947cee190d4f4e4694e8585e37bde68b04ecd

---
+++ Android.mk
@@ -0,0 +1,30 @@
+LOCAL_PATH:= $(call my-dir)/src/
+
+common_SRC_FILES := \
+	api.c \
+	arch-arm.c \
+	arch-arm-syscalls.c \
+	arch.c \
+	arch-x32.c \
+	arch-x32-syscalls.c \
+	arch-x86_64.c \
+	arch-x86_64-syscalls.c \
+	arch-x86.c \
+	arch-x86-syscalls.c \
+	db.c \
+	gen_bpf.c \
+	gen_pfc.c \
+	hash.c
+
+common_CFLAGS := -std=gnu89 -fvisibility=hidden ## -fomit-frame-pointer
+common_C_INCLUDES += $(LOCAL_PATH)/../include
+
+include $(CLEAR_VARS)
+LOCAL_CLANG := true
+LOCAL_SRC_FILES := $(common_SRC_FILES)
+LOCAL_CFLAGS += $(common_CFLAGS) -ftrapv
+LOCAL_C_INCLUDES += $(common_C_INCLUDES)
+
+LOCAL_MODULE:= libseccomp
+
+include $(BUILD_STATIC_LIBRARY)
